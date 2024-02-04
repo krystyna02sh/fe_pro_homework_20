1f6c03405ed976c2686bcde43b1a4e54ab26a94b
Krystyna Shunkina <christina.shunkina@gmail.com>
2024-02-04 23:23:09 +0200

    hw20 2 try

--- js/index.js
+++ js/index.js
@@ -1,10 +1,3 @@
-const balances = ['$123.10', '$61 123.23', '$1,321,123.10', '$0.00', '$799.50', '$1 916 161 123.25']
-const balanceNums = balances.map(
-    balance => {
-        const numbers = balance.match(/[\d\.,]+/g);
-        const number = parseFloat(numbers.join(''));
-        return number;
-
-    });
-
-console.log(balanceNums);
+const balances = ['$123.10', '$61 123.23', '$1,321,123.10', '$0.00', '$799.50', '$1 916 161 123.25'];
+const balanceNums = balances.map(balance => Number(balance.replace(/[^\d.]/g, '')));
+console.log(balanceNums); 
